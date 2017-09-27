93b2f8866faac98d619821e780cbfe9abca307ff
Mac User <sabbey37@gmail.com>
2017-09-27 11:33:48 -0400

    Creates Sequelize connection

--- sequelize.js
+++ sequelize.js
@@ -2,21 +2,7 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 const pg = require('pg');
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, {
-//   host: process.env.DB_HOST,
-//   dialect: 'postgres',
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   }
-// });
 
 const sequelize = new Sequelize(`postgres://${process.env.DB_USER}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`);
-
-
-
-// `postgres://${process.env.DB_HOST}/user:${process.env.DB_USER}:5432/${process.env.DB_NAME}`);
 
 module.exports = sequelize;
