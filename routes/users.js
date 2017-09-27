8407047a316295154f5f511ff0e1733000e93b33
Mac User <sabbey37@gmail.com>
2017-09-27 11:33:13 -0400

    Fixes sequelize

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const sequelize = require('../sequelize.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	// Comment out this line:
   //res.send('respond with a resource');
+  sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
   // And insert something like this instead:
   res.json([{
