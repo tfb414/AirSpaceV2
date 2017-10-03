49c598e1b5addeeccd6694b526aefd59a3a0c195
Natalie Villasana <navillasa@gmail.com>
2017-10-03 14:29:54 -0400

    adds nav menu BaseStyles component and MenuFactory component

--- client/src/components/HDNavBar.js
+++ client/src/components/HDNavBar.js
@@ -1,18 +1,88 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import HDNavButton from './HDNavButton'
+import HDNavButton from './HDNavButton';
 
-const HDNavBar = (props) => {
-    let navBar = props.name.map((link) => {
-        return <HDNavButton className={props.className} match={props.match} name={link} />
-    })
-    return (
-        <div className={props.className}>
-            {navBar}
-        </div>
+import menuFactory from './MenuFactory';
 
-    )
-}
+const styles = {
 
-export default HDNavBar
+  menuWrap(isOpen, width, right) {
+    return {
+      visibility: isOpen ? 'visible' : 'hidden',
+      MozTransform: 'translate3d(0, 0, 0)',
+      MsTransform: 'translate3d(0, 0, 0)',
+      OTransform: 'translate3d(0, 0, 0)',
+      WebkitTransform: 'translate3d(0, 0, 0)',
+      transform: 'translate3d(0, 0, 0)',
+      zIndex: 1,
+    };
+  },
+
+  overlay(isOpen, width, right) {
+    return {
+      zIndex: 4,
+      MozTransform: isOpen ? right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      MsTransform: isOpen ? right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      OTransform: isOpen ? right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      WebkitTransform: isOpen ? right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      transform: isOpen ? right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      transition: 'all 0.5s',
+      visibility: isOpen ? 'visible' : 'hidden'
+    };
+  },
+
+  pageWrap(isOpen, width, right) {
+    return {
+      MozTransform: isOpen ? '' : right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)`,
+      MsTransform: isOpen ? '' : right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)`,
+      OTransform: isOpen ? '' : right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)`,
+      WebkitTransform: isOpen ? '' : right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)`,
+      transform: isOpen ? '' : right ? `translate3d(-${width}, 0, 0)` : `translate3d(${width}, 0, 0)`,
+      transition: 'all 0.5s',
+      zIndex: 2,
+      position: 'relative'
+    };
+  },
+
+  burgerIcon(isOpen, width, right) {
+    return {
+      MozTransform: isOpen ? right ? `translate3d(${width}, 0, 0)` : `translate3d(-${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      MsTransform: isOpen ? right ? `translate3d(${width}, 0, 0)` : `translate3d(-${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      OTransform: isOpen ? right ? `translate3d(${width}, 0, 0)` : `translate3d(-${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      WebkitTransform: isOpen ? right ? `translate3d(${width}, 0, 0)` : `translate3d(-${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      transform: isOpen ? right ? `translate3d(${width}, 0, 0)` : `translate3d(-${width}, 0, 0)` : 'translate3d(0, 0, 0)',
+      transition: 'all 0.1s',
+      position: 'relative',
+      zIndex: 3
+    };
+  },
+
+  outerContainer(isOpen) {
+    return {
+      overflow: isOpen ? '' : 'hidden'
+    };
+  }
+};
+
+export default menuFactory(styles);
+
+// class HdNavBar extends Component {
+//     constructor (props) {
+//         super(props);
+//         this.state = {}
+// }
+
+// const HDNavBar = (props) => {
+//     let navBar = props.name.map((link) => {
+//         return <HDNavButton className={props.className} match={props.match} name={link} />
+//     })
+//     return (
+//         <div className={props.className}>
+//             {navBar}
+//         </div>
+
+//     )
+// }
+
+// export default HDNavBar
