b8ce32ca859f734efb952a9dac0c010306523cd0
Mac User <sabbey37@gmail.com>
2017-10-04 13:05:40 -0400

    removes host_id from createquiz page

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -11,9 +11,9 @@ const Create = (props) => {
             {/* <NavLink to={`/host/Create/survey`}> Survey </NavLink>
             <NavLink to={`/host/Create/quiz`}> Quiz </NavLink> */}
             <Switch>
-                <Route exact path="/host/Create" component={(host_id) => <CreateQuizSurvey {...props}/>}/>
-                <Route path="/host/Create/Quiz" component={(host_id) => <CreateQuiz {...props} />}/>
-                <Route path="/host/Create/Survey" component={(host_id) => <CreateSurvey {...props} />} />
+                <Route exact path="/host/Create" component={() => <CreateQuizSurvey {...props}/>}/>
+                <Route path="/host/Create/Quiz" component={() => <CreateQuiz {...props} />}/>
+                <Route path="/host/Create/Survey" component={() => <CreateSurvey {...props} />} />
             </Switch>
         </div>
 
