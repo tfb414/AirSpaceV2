99323f9e44d633b2257de7fe3a1ed7df65689eea
Tim Brady <tfb414@gmail.com>
2017-10-09 09:59:09 -0400

    readies the ec2 deploy branch

--- client/src/utility/env.js
+++ client/src/utility/env.js
@@ -1,3 +1,3 @@
-const WEBSOCKET = 'ws://localhost:8080'
+const WEBSOCKET = 'ws://ec2-13-59-187-26.us-east-2.compute.amazonaws.com:8080'
 export default WEBSOCKET;
 
---
+++ deleteme.js
@@ -0,0 +1,17 @@
+DB_HOST=localhost
+DB_USER=ubuntu
+DB_NAME=airspace
+DB_PASS=derp11
+DB_PORT=5432
+
+ROLE=ubuntu
+
+GOOGLE_CLIENT_ID='456569075688-fomb6ee4r74j6irkqur9p0n266st2id7.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET='kwDAHUiibpv7qQ_FsVwVeRP7'
+GOOGLE_API_KEY='AIzaSyAl0xOfRPWsJ1TZvICQ8z_pb4ynJTxePPE'
+GOOGLE_CALLBACK_HOST='http://ec2-13-59-187-26.us-east-2.compute.amazonaws.com:9999/host/auth/google/callback'
+GOOGLE_CALLBACK_GUEST='http://ec2-13-59-187-26.us-east-2.compute.amazonaws.com:9999/guest/auth/google/callback'
+
+
+
+
