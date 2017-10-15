8d1cc1e89fc1b994640c16110481bd53e574b5a9
Mac User <sabbey37@gmail.com>
2017-10-15 18:16:52 -0400

    Puts activated/not activated message under survey that was/was not activated

--- client/src/components/HostRenderSurvey.js
+++ client/src/components/HostRenderSurvey.js
@@ -8,6 +8,7 @@ class HostRenderSurvey extends Component {
         super(props);
         this.state = {
             activatedMessage: "",
+            activatedsq_id: 0,
             refresh: 0,
             waitingOnData: true
         }
@@ -44,8 +45,15 @@ class HostRenderSurvey extends Component {
             )
         }
         let surveys = this.state.results.map((data) => {
+            let activatedTR;
+            if (data.sq_id === Number(this.state.activatedsq_id)) {
+                console.log('did the thing!');
+                activatedTR = <tr><td colSpan="2"><ActivateSurvey message={this.state.activatedMessage} /></td></tr>
+            } else {
+                activatedTR = "";
+            }
             return (
-
+                <div>
                 <tr className="SQFunctions">
                     <td className="SQTitle">
                         <p>{data.sq_name}</p>
@@ -58,6 +66,8 @@ class HostRenderSurvey extends Component {
                         <button type="button" className="btn btn-outline-secondary" value={data.sq_id} onClick={this._deleteSQ}>Delete</button>
                     </td>
                 </tr>
+                {activatedTR}
+                </div>
             )
         })
         let title;
@@ -70,7 +80,7 @@ class HostRenderSurvey extends Component {
             <div className="SQComponent">
                 <div className="HostSQRenderTitleActivate">
                     <h1 className="HostSQRenderTitle">{title}</h1>
-                    <ActivateSurvey message={this.state.activatedMessage} />
+                    {/*<ActivateSurvey message={this.state.activatedMessage} />*/}
                 </div>
                 <hr className="TitleHR" />
                 <table className="HostSQTable">
@@ -82,18 +92,18 @@ class HostRenderSurvey extends Component {
 
     _viewResults = (event) => {
         if (this.props.sqtype === 'survey') {
-            this.props.history.push(`/Host/Your Surveys/${event.target.value}`)
+            this.props.history.push(`/Host/Your Surveys/${event.target.value}`);
         } else if (this.props.sqtype === 'quiz') {
-            this.props.history.push(`/Host/Your Quizzes/${event.target.value}`)
+            this.props.history.push(`/Host/Your Quizzes/${event.target.value}`);
         }
 
     }
 
     _editSQ = (event) => {
         if (this.props.sqtype === 'survey') {
-            this.props.history.push(`/Host/Your Surveys/Edit/${event.target.value}`)
+            this.props.history.push(`/Host/Your Surveys/Edit/${event.target.value}`);
         } else if (this.props.sqtype === 'quiz') {
-            this.props.history.push(`/Host/Your Quizzes/Edit/${event.target.value}`)
+            this.props.history.push(`/Host/Your Quizzes/Edit/${event.target.value}`);
         }
     }
 
@@ -105,9 +115,9 @@ class HostRenderSurvey extends Component {
         payload = JSON.stringify(payload);
         this.props.sendMessage(payload);
         if (this.props.sqtype === 'survey') {
-            this.props.history.push(`/Host/Your Surveys`)
+            this.props.history.push(`/Host/Your Surveys`);
         } else if (this.props.sqtype === 'quiz') {
-            this.props.history.push(`/Host/Your Quizzes`)
+            this.props.history.push(`/Host/Your Quizzes`);
         }
     }
 
@@ -116,7 +126,7 @@ class HostRenderSurvey extends Component {
             type: "ACTIVATESQ",
             sqtype: this.props.sqtype,
             sq_id: event.target.value
-        }
+        };
         payload = JSON.stringify(payload);
         this.props.sendMessage(payload);
     }
@@ -124,22 +134,22 @@ class HostRenderSurvey extends Component {
     _receiveMessage = (parsedData) => {
         if (parsedData.type === 'DISPLAYSQLIST' && this.props.host_id === parsedData.host_id) {
             let results = parsedData.payload;
-            console.log(results);
             this.setState({
                 waitingOnData: false,
                 results: results
-            })
+            });
         } else if (parsedData.type === "ACTIVATEDSQ" && this.props.host_id === parsedData.host_id) {
             let results = parsedData;
-            console.log(results)
             if (results.error === null) {
                 this.setState({
                     refresh: 0,
+                    activatedsq_id: results.sq_id,
                     activatedMessage: `${results.title} has been successfully activated`
                 })
             } else if (results.error === "No questions found") {
                 this.setState({
                     refresh: 0,
+                    activatedsq_id: results.sq_id,
                     activatedMessage: 'Could not activate. No questions found for that survey'
                 })
             }
