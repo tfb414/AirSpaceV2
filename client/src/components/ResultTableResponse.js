31456a1f4b56f89549c720a8bbd45589d35e7d30
Mac User <sabbey37@gmail.com>
2017-10-15 19:43:06 -0400

    Adds score row

--- client/src/components/ResultTableResponse.js
+++ client/src/components/ResultTableResponse.js
@@ -2,15 +2,25 @@ import React from 'react';
 
 const ResultTableResponse = ({ question }) => {
     let keys = Object.keys(question);
+    let scores = [];
     let tableData = keys.map((data) => {
         let responses = question[data]
+        let totalQuestions = data.length;
+        let correct = 0;
         let singleResponse = responses.map((info) => {
+            if(info.value) {
+                correct += 1;
+            }
             return (
                 <td>
                     <p className={String(info.value)}>{info.text}</p>
                 </td>
             )
         })
+        let score = Math.round(correct / totalQuestions * 100);
+        score = String(score) + "%";
+        console.log(score);
+        scores.push(score);
         return (
         <tr>
             <td className='questionBox'>
@@ -20,9 +30,18 @@ const ResultTableResponse = ({ question }) => {
         </tr>    
         )
     })
+    let scoreCells = scores.map(score => {
+        return (
+            <td>{score}</td>
+        )
+    })
     return (
         <tbody>
             {tableData}
+            <tr>
+                <td>Score</td>
+                {scoreCells}
+            </tr>
         </tbody>
     );
 };
