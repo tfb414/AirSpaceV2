b2bc6a5c722ab4fd194156ce24b472908f0825f8
Mac User <sabbey37@gmail.com>
2017-10-17 13:21:06 -0400

    Renders about page

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const sessionmanager = require('./sessionmanager');
 const queries = require('./queries');
 require('dotenv').config();
 const ensureAuthenticated = require('./utils').ensureAuthenticated;
-var about = require('./routes/about');
 var websocket = require('./websocket');
 var query = require('./queries');
 
@@ -79,6 +78,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // app.use('/', index);
+// app.use('/About/', about);
 
 app.get('/host/auth/google',
     passport.authenticate('host', { scope: ['profile', 'email'] }));
@@ -99,8 +99,9 @@ app.get('/guest/auth/google/callback',
         failureRedirect: '/',
         failureFlash: true
     }));
-
-app.use('/About/', about);
+app.use('/About/', (req, res) => {
+    res.sendFile('/public/about.html', { "root": __dirname });
+});
 
 app.get('*', ensureAuthenticated, (req, res, next) => {
     res.sendFile('/public/index.html', { "root": __dirname });
